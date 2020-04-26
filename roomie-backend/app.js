7a15a5c7797fe3d5d8da1a76f14a800813fdb7f2
lucasmrl <nutnewbr@gmail.com>
2020-04-25 23:38:06 -0600

    Added env variables/ Installed dotenv

--- roomie-backend/app.js
+++ roomie-backend/app.js
@@ -6,7 +6,9 @@ const listingRouter = require("./routes/listingRouter");
 const userRouter = require("./routes/userRouter");
 
 //MIDDLEWARES
-app.use(morgan("dev")); //HTTP Request logger
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan("dev")); //HTTP Request logger
+}
 app.use(express.json()); //Makes the data from the body to be added to the "req" object
 
 //ROUTES
--- roomie-backend/server.js
+++ roomie-backend/server.js
@@ -1,6 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
