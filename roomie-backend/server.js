fb7e423dbc214c40930c3a76384a68f8a35e1b9e
lucasmrl <nutnewbr@gmail.com>
2020-04-26 01:03:47 -0600

    Created and connect database

--- roomie-backend/server.js
+++ roomie-backend/server.js
@@ -1,8 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
+mongoose
+  .connect(process.env.DATABASE, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful!🔑');
+  })
+  .catch((err) => {
+    console.log(`DB Connection Error: ${err.message}❌`);
+  });
+
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
+  console.log(`App running on port ${port}...🚀`);
 });
