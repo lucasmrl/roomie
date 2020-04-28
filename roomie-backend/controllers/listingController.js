f4e05441dbabd2dcc2a0c8165e5f996dea1714de
lucasmrl <nutnewbr@gmail.com>
2020-04-27 23:36:56 -0600

    API - Implement Filtering

--- roomie-backend/controllers/listingController.js
+++ roomie-backend/controllers/listingController.js
@@ -2,7 +2,18 @@ const Listing = require('./../models/listingModel');
 
 exports.getAllListings = async (req, res) => {
   try {
-    const listings = await Listing.find(); //Return an array w/ all the documents
+    /* In order to allow future developemnt of pagination, sorting, etc, I:
+     * 1)Made a hard copy of the Query Object
+     * 2)Create a array of the fields that I want to exclude from the queryObj
+     * 3)Used a forEach (to not create a new array) in order to remove these fields from the Oject
+     * that will be used in the .find()
+     */
+    const queryObj = { ...req.query }; //Copy of the Query Obj
+    const excludeFields = ['page', 'sort', 'limit', 'fields'];
+    excludeFields.forEach((el) => delete queryObj[el]);
+
+    const listings = await Listing.find(queryObj); //find() = Return an array w/ all the documents
+    //"Listing.find(queryObj)" returns a query - that is why I can chain others methods (to sort, to use paginations,etc...)
 
     res.status(200).json({
       status: 200,
