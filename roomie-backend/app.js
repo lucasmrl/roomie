5973d487fd590b0d94b1602d078461786a86d4c9
lucasmrl <nutnewbr@gmail.com>
2020-05-02 04:03:55 -0600

    Implemented API limiting

--- roomie-backend/app.js
+++ roomie-backend/app.js
@@ -1,16 +1,27 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const listingRouter = require('./routes/listingRouter');
 const userRouter = require('./routes/userRouter');
 
-//MIDDLEWARES
+// GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev')); //HTTP Request logger
 }
+
+// -- Rate limiting
+const limiter = rateLimit({
+  max: 500,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour',
+}); // 100 Requestes max from the same IP in 1 hour
+
+app.use('/api', limiter);
+
 app.use(express.json()); //Makes the data from the body to be added to the "req" object
 
 //ROUTES
