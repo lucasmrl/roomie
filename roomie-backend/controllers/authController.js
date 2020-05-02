badb1b245095e58e4c14daf6bf0e6db9a8fcac6d
lucasmrl <nutnewbr@gmail.com>
2020-05-02 03:57:22 -0600

    Send JWT via cookie

--- roomie-backend/controllers/authController.js
+++ roomie-backend/controllers/authController.js
@@ -15,6 +15,20 @@ const signToken = (id) => {
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true; //Makes that it only works ith HTTPS
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from Output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'sucess',
     token, //Sending the token back to the client
