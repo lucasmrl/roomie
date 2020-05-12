26c180496f315bce4b9b92387b4c1c5b1df5f76a
lucasmrl <nutnewbr@gmail.com>
2020-05-11 21:51:58 -0600

    Added 'Signup'/ Removed countdown in Logout

--- roomie-frontend/src/components/Logout.js
+++ roomie-frontend/src/components/Logout.js
@@ -1,29 +1,29 @@
-import React, { useContext, useEffect } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { AuthContext } from "./../context/AuthContext";
 import axios from "axios";
 
 function Logout(props) {
   const { setIsAuth } = useContext(AuthContext);
 
-  //   useEffect(() => {
-  setInterval(async () => {
-    try {
-      const response = await axios({
-        method: "GET",
-        url: "/api//users/logout",
-      });
-      //User will receive an invalid token as the response
+  useEffect(() => {
+    setInterval(async () => {
+      try {
+        const response = await axios({
+          method: "GET",
+          url: "/api//users/logout",
+        });
+        //User will receive an invalid token as the response
 
-      if (response.status === 200) {
-        setIsAuth(false); // 1) When the user is redirected, the navbar will be updated
-        localStorage.removeItem("isAuth"); // 2) Removing the information from localStorage
-        props.history.push("/"); // 3) Redirecting to the main page with the correct navbar
+        if (response.status === 200) {
+          setIsAuth(false); // 1) When the user is redirected, the navbar will be updated
+          localStorage.removeItem("isAuth"); // 2) Removing the information from localStorage
+          props.history.push("/"); // 3) Redirecting to the main page with the correct navbar
+        }
+      } catch (error) {
+        return alert("Something went wrong! ❌");
       }
-    } catch (error) {
-      return alert("Something went wrong! ❌");
-    }
-  }, 3000);
-  //   }, []);
+    }, 2000);
+  }, []);
 
   return <p>You will be disconnected in 3 seconds... 😢</p>;
 }
--- roomie-frontend/src/components/Signup.js
+++ roomie-frontend/src/components/Signup.js
@@ -1,10 +1,81 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { AuthContext } from "./../context/AuthContext";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 
-function Signup() {
+function Signup(props) {
+  const { setIsAuth } = useContext(AuthContext);
+
+  const { register, handleSubmit, watch, errors } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios({
+        method: "POST",
+        url: "/api/users/signup",
+        data,
+      });
+
+      if (response.status === 201) {
+        localStorage.setItem("isAuth", "true");
+        setIsAuth(true);
+        props.history.push("/");
+      }
+    } catch (error) {
+      return alert("Sorry, We could not sign you up! ❌");
+    }
+  };
+
+  console.log(watch("password"));
+
   return (
     <div>
-      <p>Sign Up here, please.</p>
+      <h2>Sign Up:</h2>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label>Name:</label>
+        <input
+          type="text"
+          placeholder="First and Last Name"
+          name="name"
+          ref={register({ required: true })}
+        />
+        {errors.name && <span>Please provide a Title.</span>}
+
+        <label>E-mail:</label>
+        <input
+          type="email"
+          placeholder="Email"
+          name="email"
+          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <span>Please provide a valid email.</span>}
+
+        <label>Password:</label>
+        <input
+          type="password"
+          placeholder=""
+          name="password"
+          ref={register({ required: true, minLength: 8 })}
+        />
+        {errors.password && (
+          <span>Please provide a password with at least 8 characters.</span>
+        )}
+
+        <label>Confirm Password:</label>
+        <input
+          type="password"
+          placeholder=""
+          name="passwordConfirm"
+          ref={register({
+            required: true,
+            validate: (value) => value === watch("password"),
+          })}
+        />
+        {errors.passwordConfirm && (
+          <span>Please provide the same password above</span>
+        )}
+        <input type="submit" />
+      </form>
     </div>
   );
 }
