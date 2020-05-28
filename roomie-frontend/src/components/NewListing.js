c8822bc877717f98b75708bf6224cd7d15ab0cf7
lucasmrl <nutnewbr@gmail.com>
2020-05-27 23:43:14 -0600

    FE: Finished "NewListings" page

--- roomie-frontend/src/components/NewListing.js
+++ roomie-frontend/src/components/NewListing.js
@@ -52,156 +52,273 @@ function NewListing() {
     }
   };
   return (
-    <div>
-      <h2>New Listing:</h2>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <label>Images:</label>
-        <input type="file" name="pictures0" ref={register} />
-        <input type="file" name="pictures1" ref={register} />
-        <input type="file" name="pictures2" ref={register} />
+    <div className="flex flex-col">
+      {/* Header */}
+      <div className="px-6 py-3 bg-themeGreen">
+        <h1 className="font-bold text-2xl text-gray-900">New Listing</h1>
+      </div>
+      {/* Form */}
+      <div className="flex flex-col p-6">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="font-bold text-gray-900 mt-3">Images:</label>
+          <input
+            type="file"
+            name="pictures0"
+            ref={register}
+            className="block border-4 border-dotted my-2"
+          />
+          <input
+            type="file"
+            name="pictures1"
+            ref={register}
+            className="block border-4 border-dotted my-2"
+          />
+          <input
+            type="file"
+            name="pictures2"
+            ref={register}
+            className="block border-4 border-dotted my-2"
+          />
 
-        <label>Title:</label>
-        <input
-          type="text"
-          placeholder="Title"
-          name="title"
-          ref={register({ required: true, maxLength: 80 })}
-        />
-        {errors.title && <span>Please provide a Title.</span>}
-        <label>Room:</label>
-        <select name="type" ref={register({ required: true })}>
-          <option value="shared">Shared</option>
-          <option value="private">Private</option>
-        </select>
-        <label>Building Type:</label>
-        <select name="buildingType" ref={register({ required: true })}>
-          <option value="home">Home</option>
-          <option value="basement">Basement</option>
-          <option value="apartment">Apartment</option>
-          <option value="condo">Condo</option>
-          <option value="townhome">Townhome</option>
-        </select>
-        <label>Rent:</label>
-        <input
-          type="number"
-          placeholder="Value"
-          name="rent"
-          ref={register({ required: true })}
-        />
-        {errors.rent && <span>Please provide the Rent.</span>}
-        <label>Utilities Included?:</label>
-        <input
-          className="radio"
-          name="utilitiesIncl"
-          type="radio"
-          value="true"
-          ref={register({ required: true })}
-        />
-        <label className="radio">Yes</label>
+          <p className="font-bold text-gray-900 mt-4 text-xl">
+            Listing Information:
+          </p>
+          <hr className="border-orange-300 mb-4" />
 
-        <input
-          className="radio"
-          name="utilitiesIncl"
-          type="radio"
-          value="false"
-          ref={register({ required: true })}
-        />
-        <label className="radio">No</label>
-        {errors.utilitiesIncl && (
-          <span>Please inform if the utilities are included.</span>
-        )}
+          <label className="font-bold text-gray-900 mt-3">Title:</label>
+          <input
+            type="text"
+            placeholder=""
+            name="title"
+            ref={register({ required: true, maxLength: 80 })}
+            className="block my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline sm:w-full sm:flex-grow"
+          />
+          {errors.title && (
+            <span className="my-2 text-red-600">Please provide a Title.</span>
+          )}
 
-        <label>Address:</label>
-        <input
-          type="text"
-          placeholder="Address"
-          name="address"
-          ref={register({ required: true })}
-        />
-        {errors.address && <span>Please provide a Address.</span>}
-        <label>City:</label>
-        <input
-          type="text"
-          placeholder="City"
-          name="city"
-          ref={register({ required: true })}
-        />
-        {errors.city && <span>Please provide a City.</span>}
-        <label>State:</label>
-        <input
-          type="text"
-          placeholder="State"
-          name="state"
-          ref={register({ required: true, maxLength: 2 })}
-        />
-        {errors.state && <span>Please provide a State.</span>}
-        <label>Zip:</label>
-        <input
-          type="number"
-          placeholder="Zipcode"
-          name="zip"
-          ref={register({ required: true, maxLength: 5 })}
-        />
-        {errors.zip && <span>Please provide a Zip.</span>}
-        <label>Country:</label>
-        <input
-          type="text"
-          placeholder="Country"
-          name="country"
-          ref={register({ required: true })}
-        />
-        {errors.country && <span>Please provide a Country.</span>}
-        <label>Description about your space:</label>
-        <textarea name="description" ref={register({ required: true })} />
-        {errors.description && <span>Please provide a Description.</span>}
-        <label>First Date Available:</label>
-        <input
-          type="date"
-          placeholder="Available Date"
-          name="availableDate"
-          ref={register({ required: true })}
-        />
-        {errors.availableDate && (
-          <span>Please provide the first available date.</span>
-        )}
-        <label>Pets Allowed?</label>
-        <input
-          className="radio"
-          name="petAllowed"
-          type="radio"
-          value="false"
-          ref={register({ required: true })}
-        />
-        <label className="radio">No</label>
-        <input
-          className="radio"
-          name="petAllowed"
-          type="radio"
-          value="true"
-          ref={register({ required: true })}
-        />
-        <label className="radio">Yes</label>
-        {errors.petAllowed && <span>Please inform if Pets are allowed.</span>}
-        <label>Contact E-mail:</label>
-        <input
-          type="email"
-          placeholder="Email"
-          name="contactEmail"
-          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-        />
-        {errors.contactEmail && (
-          <span>Please provide a e-mail for contact.</span>
-        )}
-        <label>Contact Phone:</label>
-        <input
-          type="tel"
-          placeholder="Mobile number"
-          name="contactPhone"
-          ref={register({ required: true, minLength: 6, maxLength: 12 })}
-        />
-        {errors.contactPhone && <span>Please provide a contact phone.</span>}
-        <input type="submit" />
-      </form>
+          <div className="lg:flex lg:justify-start">
+            <div>
+              <label className="block font-bold text-gray-900 mt-3">
+                Room:
+              </label>
+              <select
+                name="type"
+                ref={register({ required: true })}
+                className="block my-2 text-xl"
+              >
+                <option value="shared">Shared</option>
+                <option value="private">Private</option>
+              </select>
+            </div>
+
+            <div className="lg:mt-3 lg:ml-8">
+              <label className="font-bold text-gray-900 mt-3">
+                Building Type:
+              </label>
+              <select
+                name="buildingType"
+                ref={register({ required: true })}
+                className="block my-2 text-xl"
+              >
+                <option value="home">Home</option>
+                <option value="basement">Basement</option>
+                <option value="apartment">Apartment</option>
+                <option value="condo">Condo</option>
+                <option value="townhome">Townhome</option>
+              </select>
+            </div>
+
+            <div className="lg:ml-8">
+              <label className="block font-bold text-gray-900 mt-3">
+                Utilities Included?
+              </label>
+              <input
+                className="radio"
+                name="utilitiesIncl"
+                type="radio"
+                value="true"
+                ref={register({ required: true })}
+                className="inline ml-2"
+              />
+              <label className="text-xl mx-1">Yes</label>
+
+              <input
+                className="radio"
+                name="utilitiesIncl"
+                type="radio"
+                value="false"
+                ref={register({ required: true })}
+                className="inline ml-2 text-xl"
+              />
+              <label className="text-xl mx-1">No</label>
+              {errors.utilitiesIncl && (
+                <span className="block my-2 text-red-600">
+                  Please inform if the utilities are included.
+                </span>
+              )}
+            </div>
+            <div className="lg:ml-8">
+              <label className="block font-bold text-gray-900 mt-3">
+                Pets Allowed?
+              </label>
+              <input
+                className="radio"
+                name="petAllowed"
+                type="radio"
+                value="false"
+                ref={register({ required: true })}
+                className="inline ml-2"
+              />
+              <label className="text-xl mx-1">No</label>
+              <input
+                className="radio"
+                name="petAllowed"
+                type="radio"
+                value="true"
+                ref={register({ required: true })}
+                className="inline ml-2 text-xl"
+              />
+              <label className="text-xl mx-1">Yes</label>
+              {errors.petAllowed && (
+                <span className="block my-2 text-red-600">
+                  Please inform if Pets are allowed.
+                </span>
+              )}
+            </div>
+          </div>
+
+          <label className="font-bold text-gray-900 mt-3">Rent:</label>
+          <input
+            type="number"
+            placeholder="$"
+            name="rent"
+            ref={register({ required: true })}
+            className="block my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.rent && (
+            <span className="my-2 text-red-600">Please provide the Rent.</span>
+          )}
+
+          <label className="block font-bold text-gray-900 mt-3">Address:</label>
+          <input
+            type="text"
+            placeholder=""
+            name="address"
+            ref={register({ required: true })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.address && (
+            <span className="my-2 text-red-600">Please provide a Address.</span>
+          )}
+          <label className="block font-bold text-gray-900 mt-3">City:</label>
+          <input
+            type="text"
+            placeholder=""
+            name="city"
+            ref={register({ required: true })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.city && (
+            <span className="my-2 text-red-600">Please provide a City.</span>
+          )}
+          <label className="block font-bold text-gray-900 mt-3">State:</label>
+          <input
+            type="text"
+            placeholder="XX"
+            name="state"
+            ref={register({ required: true, maxLength: 2 })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.state && (
+            <span className="my-2 text-red-600">Please provide a State.</span>
+          )}
+          <label className="block font-bold text-gray-900 mt-3">Zip:</label>
+          <input
+            type="number"
+            placeholder="XXXXX"
+            name="zip"
+            ref={register({ required: true, maxLength: 5 })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.zip && (
+            <span className="my-2 text-red-600">Please provide a Zip.</span>
+          )}
+          <label className="block font-bold text-gray-900 mt-3">Country:</label>
+          <input
+            type="text"
+            placeholder=""
+            name="country"
+            ref={register({ required: true })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.country && (
+            <span className="my-2 text-red-600">Please provide a Country.</span>
+          )}
+          <label className="block font-bold text-gray-900 mt-3">
+            Description about your space:
+          </label>
+          <textarea
+            name="description"
+            ref={register({ required: true })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.description && (
+            <span className="my-2 text-red-600">
+              Please provide a Description.
+            </span>
+          )}
+          <label className="block font-bold text-gray-900 mt-3">
+            First Date Available:
+          </label>
+          <input
+            type="date"
+            placeholder="Available Date"
+            name="availableDate"
+            ref={register({ required: true })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.availableDate && (
+            <span className="my-2 text-red-600">
+              Please provide the first available date.
+            </span>
+          )}
+
+          <p className="font-bold text-gray-900 mt-4 text-xl">Contact:</p>
+          <hr className="border-orange-300 mb-4" />
+
+          <label className="block font-bold text-gray-900 mt-3">E-mail:</label>
+          <input
+            type="email"
+            placeholder="email@email.com"
+            name="contactEmail"
+            ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.contactEmail && (
+            <span className="my-2 text-red-600">
+              Please provide a e-mail for contact.
+            </span>
+          )}
+          <label className="block font-bold text-gray-900 mt-3">Phone:</label>
+          <input
+            type="tel"
+            placeholder="(XXX) XXX - XXXX"
+            name="contactPhone"
+            ref={register({ required: true, minLength: 6, maxLength: 12 })}
+            className="my-2 shadow p-1 appearance-none text-xl border lg:text-xl lg:px-4 rounded-lg text-gray-700 focus:outline-none focus:shadow-outline md:w-full md:flex-grow"
+          />
+          {errors.contactPhone && (
+            <span className="my-2 text-red-600">
+              Please provide a contact phone.
+            </span>
+          )}
+          <input
+            type="submit"
+            className="block my-2 md:inline bg-themeYellow mx-1 px-3 lg:mt-6 py-1 lg:text-2xl rounded-lg text-xl text-gray-800 focus:outline-none focus:shadow-outline shadow"
+          />
+        </form>
+      </div>
     </div>
   );
 }
